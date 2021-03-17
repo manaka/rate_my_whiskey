c6dd5a6eed2a997669db7ddace2e7d45eedf1786
manaka <romeod@gmail.com>
2021-03-17 13:24:02 +0100

    Added search API

---
+++ app/controllers/api/v1/search_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SearchController < Api::V1::BaseController
+  def index
+    @whiskeys = if params[:q].blank?
+                  Whiskey.includes(:scores).all
+                else
+                  Whiskey.includes(:scores).search(params[:q]).all
+                end
+
+    respond_to do |format|
+      format.json { render json: @whiskeys.as_json(include: [:scores]) }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :whiskeys
   resources :score_types
+  namespace :api do
+    namespace :v1 do
+      resources :search, only: [:index]
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
