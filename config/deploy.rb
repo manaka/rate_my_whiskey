b222f7b0b22fa6f26198f096877aaedf3727a7df
manaka <romeod@gmail.com>
2021-03-18 16:24:57 +0100

    Added capistrano configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'rate_my_whiskey'
+set :repo_url, 'git@github.com:manaka/rate_my_whiskey.git'
+set :deploy_to, '/var/www/rails'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :keep_releases, 3
+
+append :linked_files, "config/master.key"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
